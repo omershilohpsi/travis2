86e43b3488346c23adac498e71c9785f422e1c0b
Omer Gilad Shiloh <omer_psi@webiks.com>
2021-05-27 14:08:13 +0300

    Fixed app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-import json
-class Dog:
+"""This module is an attempt to practise class in python."""
 
+
+class Dog:
+    """A simple attempt to model a dog."""
 
     def __init__(self, name, age):
         """Initialize name and age attributes."""
@@ -9,12 +11,13 @@ class Dog:
         self.name = "Rambo"
         self.age = 12
 
-
     def sit(self):
+        """Simulate a dog sitting in response to a command."""
         print(f"{self.dog_name} is now sitting")
 
-
     def roll_over(self):
+        """Simulate a dog sitting in response to a command."""
         print(f"{self.dog_age} rolled over!")
+
 
 my_dog = Dog("Maxwell", 6)
